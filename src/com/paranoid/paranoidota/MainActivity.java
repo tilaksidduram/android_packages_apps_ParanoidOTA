a03fe86052bdd4adfe7f9ee80fed4a24b97a88cb
Joe Wooster <jeebobs@btinternet.com>
2014-10-01 19:32:37 +0100

    ParanoidOTA: Fix Changelog URL format
    The current URL format only allows the changelog to be opened
    with a web browser, this change allows it to be opened with Google+.
    
    Change-Id: Ib858d94da7a8460c90a8406157f7a2eeea3f40e3
    
    Conflicts:
    	src/com/paranoid/paranoidota/MainActivity.java

--- src/com/paranoid/paranoidota/MainActivity.java
+++ src/com/paranoid/paranoidota/MainActivity.java
@@ -70,8 +70,8 @@ import java.util.List;
 public class MainActivity extends Activity implements UpdaterListener, DownloadCallback,
         OnItemClickListener {
 
-    private static final String CHANGELOG = "https://plus.google.com/app/basic/106020481530876935386/posts";
-    private static final String GOOGLEPLUS = "https://plus.google.com/communities/112507607742650442080";
+    private static final String CHANGELOG = "https://plus.google.com/106020481530876935386/posts";
+    private static final String GOOGLEPLUS = "https://plus.google.com/communities/103106032137232805260";
     private static final String STATE = "STATE";
 
     public static final int STATE_UPDATES = 0;
